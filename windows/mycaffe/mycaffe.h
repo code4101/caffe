69f1d0b25e14240b31eb5b7128d4412e69898abc
code4101 <877362867@qq.com>
2016-11-07 15:49:34 +0800

    mycaffe.dll动态库接口，mytest使用测试成功

---
+++ windows/mycaffe/mycaffe.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <string>
+#include <opencv2/core/core.hpp>
+using std::string;
+
+class CCaffeCfr {
+public: //构造函数
+	_declspec(dllexport) CCaffeCfr();
+	_declspec(dllexport) CCaffeCfr(string deploy_prototxt, string network_caffemodel, string mean_binaryproto, string labels_txt);
+	_declspec(dllexport) void reset(string deploy_prototxt, string network_caffemodel, string mean_binaryproto, string labels_txt);
+
+	_declspec(dllexport) ~CCaffeCfr();
+
+public: //分类函数
+	_declspec(dllexport) string cfr(cv::Mat img) const;
+};
---
+++ windows/mytest/main.cpp
@@ -0,0 +1,27 @@
+﻿#include "../mycaffe/mycaffe.h"
+#include "iostream"
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+
+#ifdef _DEBUG
+#pragma comment(lib, "E:/slns/caffe/Build/x64/Debug/mycaffed")
+#else
+#pragma comment(lib, "E:/slns/caffe/Build/x64/Release/mycaffe")
+#endif
+
+int main() {
+	CCaffeCfr cfr("lenet.prototxt", "lenet_iter_10000.caffemodel", "mean.binaryproto", "labels.txt");
+
+	Mat img;
+	
+	for (int i = 0; i < 2; ++i) {
+		for (int j = 1; j < 16; ++j) {
+			string s = format("img_num/%d_%04d.jpg", i, j);
+			img = ~imread(s);
+			cout << s << ": " << cfr.cfr(img) << "\n";
+		}
+	}
+	
+	return 0;
+}
